452a0a7e8e0d3f0bbfb4d115a0b99a46a7ce0825
Sahil1618 <ss8876584@gmail.com>
2025-11-25 14:59:27 +0530

    Add custom React and Counter Mini Project

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.jsx";
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <div>
+      <h1>Classic react vite app</h1>
+      <Youtube />
+    </div>
+  );
+}
+
+export default App;
